ab456c65967365d910ab8d1a9ca093bc95f0c8a6
hamzahakoun94 <hakprojects94@gmail.com>
2019-08-19 17:17:56 +0200

    http methods are created

---
+++ src/funcs/http.js
@@ -0,0 +1,25 @@
+import configs from '../configs' ;
+import axios from './axios' ;
+
+// Get Request
+const getRequest = (url) => {
+  const { baseUrl } = configs ;
+  const dest = baseUrl + '/' + url ;
+  return axios.get(dest) ;
+}
+
+// for both Put , Post request
+const request = (url, payload ,type = 'post') => {
+  const { baseUrl } = configs ;
+  const dest = baseUrl + '/' + url ;
+  return axios({
+    type , 
+    url : dest ,
+    data : payload ,
+  })
+}
+
+export {
+  getRequest ,
+  request
+}
